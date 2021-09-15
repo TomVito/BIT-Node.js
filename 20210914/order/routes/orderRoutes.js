3aeee8f5d55fb9d6c61ee4a5818174a470603057
TomVito <t.vitonis@gmail.com>
2021-09-15 21:59:27 +0300

    patch and remove routes

--- 20210914/order/routes/orderRoutes.js
+++ 20210914/order/routes/orderRoutes.js
@@ -1,10 +1,9 @@
 const express=require('express');
-const { get } = require('mongoose');
 const router=express.Router();
 const Order=require('../models/order');
 const Service=require('../models/service');
 
-router.get('/orders',(req,res,next)=>{
+router.get('/orders', (req,res,next)=>{
    Order.find({}).then((orders)=>{
        res.send(orders);
    }).catch((e)=>{
@@ -12,16 +11,19 @@ router.get('/orders',(req,res,next)=>{
    });
 });
 
-router.get('/orders/services/:id',(req,res,next)=>{
+router.get('/orders/services/:id', (req,res,next)=>{
    const id=req.params.id;
    Order.find({service_id:id}).then((orders)=>{
+    if (orders.length==0){
+        return res.status(404).send();
+    }
        res.send(orders);
    }).catch((e)=>{
        res.status(500).send(e);
    });
 });
 
-router.get('/orders/:id',(req,res,next)=>{
+router.get('/orders/:id', (req,res,next)=>{
    const id=req.params.id;
    Order.findById(id).then((orders)=>{
        if (!orders){
@@ -33,13 +35,49 @@ router.get('/orders/:id',(req,res,next)=>{
    });
 });
 
-router.post('/orders',(req,res,next)=>{
+router.post('/orders', async (req,res,next)=>{
    const order=new Order(req.body);
-   order.save().then(()=>{
+   const service=await Service.findById(order.service_id);
+   if (!service){
+    return res.status(404).send();
+   }
+    order.save().then(()=>{
        res.status(201).send(order);
    }).catch((e)=>{
        res.status(400).send(e);
    });
+});
+
+router.patch('/orders/:id', async (req,res,next)=>{
+    try {
+        const order=await Order.findById(req.params.id);
+        const updates=Object.keys(req.body);
+        /*
+        const allowed=['email','phone'];
+        if (!updates.every((update)=>allowed.includes(update))){
+            return res.status(400).send({error:"Wrong update fields"});
+        }
+        */
+        updates.forEach((update)=>{
+            order[update]=req.body[update];
+        });
+        await order.save();
+        res.send(order);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.delete('/orders/:id', async (req,res,next)=>{
+    try {
+        const order=await Order.findByIdAndDelete(req.params.id);
+        if (!order){
+            return res.status(404).send({error:"Record not found"})
+        }
+        return res.send(order);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 module.exports=router;
--- 20210914/order/routes/serviceRoutes.js
+++ 20210914/order/routes/serviceRoutes.js
@@ -2,7 +2,7 @@ const express=require('express');
 const router=express.Router();
 const Service=require('../models/service');
 
-router.get('/services',(req,res,next)=>{
+router.get('/services', (req,res,next)=>{
    Service.find({}).then((services)=>{
        res.send(services);
    }).catch((e)=>{
@@ -10,7 +10,7 @@ router.get('/services',(req,res,next)=>{
    });
 });
 
-router.get('/services/:id',(req,res,next)=>{
+router.get('/services/:id', (req,res,next)=>{
    const id=req.params.id;
    Service.findById(id).then((services)=>{
        if (!services){
@@ -22,13 +22,45 @@ router.get('/services/:id',(req,res,next)=>{
    });
 });
 
-router.post('/services',(req,res,next)=>{
+router.post('/services', (req,res,next)=>{
    const service=new Service(req.body);
    service.save().then(()=>{
        res.status(201).send(service);
    }).catch((e)=>{
        res.status(400).send(e);
    });
+});
+
+router.patch('/services/:id', async (req,res,next)=>{
+    try {
+        const service=await Service.findById(req.params.id);
+        const updates=Object.keys(req.body);
+        /*
+        const allowed=['price'];
+        if (!updates.every((update)=>allowed.includes(update))){
+            return res.status(400).send({error:"Wrong update fields"});
+        }
+        */
+        updates.forEach((update)=>{
+            service[update]=req.body[update];
+        });
+        await service.save();
+        res.send(service);
+    } catch (error) {
+        res.status(400).send(error);
+    }
+});
+
+router.delete('/services/:id', async (req,res,next)=>{
+    try {
+        const service=await Service.findByIdAndDelete(req.params.id);
+        if (!service){
+            return res.status(404).send({error:"Record not found"})
+        }
+        return res.send(service);
+    } catch (error) {
+        res.status(500).send(error);
+    }
 });
 
 module.exports=router;
